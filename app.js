7c117f465236a5fbf67e7061b43b7a6c257acb44
harenarium <carolinel2610@gmail.com>
2017-11-19 19:33:42 +0000

    Done.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 // Load Controllers
-const SiteController = require(./controllers/SiteController.js) //require not requires
+const SiteController = require("./controllers/SiteController.js") //require not requires and need ""
 
 // Routes
 app.get("/", SiteController.Index)
