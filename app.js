2d10d5c3dc08f0029e76a9ffbdd6914a047aafe8
sabu sabu <godugubalesabu@gmail.com>
2017-11-19 18:20:55 +0000

    Done.

--- app.js
+++ app.js
@@ -2,19 +2,16 @@ var express = require('express')
 var app = express()
 
 // Load Controllers
+app.get('/',function(req,resp){
+  const welcomeText="welcome to express module";
+  resp.render("index",{
+    "welcomeText":welcomeText,
+    name:"balesabu",
+    colors:"green"
 
-
+  });
+})
 // Routes
 
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
