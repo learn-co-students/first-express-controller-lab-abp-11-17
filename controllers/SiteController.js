e16aa43bb3dd386041e82413c366b98a47014026
harenarium <carolinel2610@gmail.com>
2017-11-20 04:14:25 +0000

    Done.

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,7 +1,11 @@
 const SiteController = {};
 
 SiteController.Index = function(req, resp){
+<<<<<<< HEAD
   resp.render('site/index',{welcomeMessage: "Welcome to the Home Page"})
+=======
+  resp.render('index')
+>>>>>>> 0bd6c9e64367af1447f9d1d9f029ee14dd2d8932
 }
 
 
