31e3c1b3c87cd7df8bdbf51375bae99b6750d76a
Rebecca Inge <rebecca.inge@gmail.com>
2017-11-20 23:52:27 +0000

    Done.

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,4 +1,15 @@
 const SiteController = {};
 
+SiteController.Index = function(req, resp){
+
+}
+
+SiteController.About = function(req, resp){
+
+}
+
+SiteController.Contact = function(req, resp){
+
+}
 
 module.exports = SiteController;
