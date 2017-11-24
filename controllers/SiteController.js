ae999d4708bbeac72fc64aaf16c53322ef084f02
Tommaso Padula <tommaso.padula@gmail.com>
2017-11-24 11:34:35 +0000

    Done.

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,4 +1,17 @@
 const SiteController = {};
 
+SiteController.Index = function(req, resp){
+  resp.render("site/index")
+} // Index Action
+
+SiteController.About = function(req, resp){
+  resp.render("site/about")
+
+} // About Action
+
+SiteController.Contact = function(req, resp){
+  resp.render("site/contact")
+
+} // Contact Action
 
 module.exports = SiteController;
