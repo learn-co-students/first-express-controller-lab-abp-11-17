e1acdeb8aa8f0beb6fd6383b228987b549ee0878
Jennifer Rhees <redswinglinestapler23@gmail.com>
2017-11-26 23:07:13 +0000

    Done.

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
 var express = require('express')
 var app = express()
 
+const SiteController = require("./controllers/SiteController")
+
+app.get("/", SiteController.Index)
+app.get("/about", SiteController.About)
+app.get("/contact", SiteController.Contact)
 
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
