d68db752e5032b9c4cfc89a74e35526fd1de3ce5
LearnAndExplore <pangpang_1993_0506@yahoo.com.hk>
2017-12-02 15:51:36 +0000

    Done.

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 var express = require('express')
 var app = express()
+var SiteController = require('./controllers/SiteController')
 
-
+app.get('/',SiteController.Index
+    // function(req,resp){
+    //     resp.render('index')
+)
+app.get('/about',SiteController.About)
+app.get('/contact',SiteController.Contact)
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
