92b6ab25c589f449a59057a4aad1aa8e2cfa676d
Quinn Dizon <qdizon@gmail.com>
2017-12-02 00:04:43 +0000

    Done.

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,4 +1,16 @@
 const SiteController = {};
 
+SiteController.Index = function(request, response){
+  response.render('site/index');
+}
+
+SiteController.About = function(request, response){
+  response.render('site/about');
+}
+
+SiteController.Contact = function(request, response){
+  response.render('site/contact');
+}
+
 
 module.exports = SiteController;
