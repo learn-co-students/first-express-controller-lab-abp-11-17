586fa5e6a863201065422f3583e854ff77802e1b
jamieberrier <berrier.jamie@gmail.com>
2017-12-04 19:51:17 +0000

    Done.

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 var express = require('express')
 var app = express()
 
+//load controllers
+const SiteController = require("./controllers/SiteController")
 
+//routes
+app.get('/', SiteController.Index)
+app.get('/about', SiteController.About)
+app.get('/contact', SiteController.Contact)
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
---
+++ controllers/SiteController.js
@@ -0,0 +1,15 @@
+const SiteController = {};
+
+SiteController.Index = function(req, resp){
+  resp.render("site/index");
+}
+
+SiteController.About = function(req, resp){
+  resp.render("site/about");
+}
+
+SiteController.Contact = function(req, resp){
+  resp.render("site/contact");
+}
+
+module.exports = SiteController;
