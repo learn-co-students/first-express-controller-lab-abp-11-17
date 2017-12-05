1d22008a41c1466848cb2512a155fd2d4a1d5cd5
meinnes <innesmercean@gmail.com>
2017-12-05 19:01:42 +0000

    Done.

---
+++ controllers/SiteController.js
@@ -0,0 +1,14 @@
+class SiteController{
+
+  static Index(req, resp){
+    resp.render('site/index');
+  }
+  static About(req, resp){
+    resp.render('site/about');
+  }
+  static Contact(req, resp){
+    resp.render('site/contact');
+  }
+
+}
+module.exports=SiteController;
