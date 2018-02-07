4779d8a110646427a08a0b2cebbb9a3a84044895
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-02-07 19:19:40 +0000

    Done.

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 var express = require('express')
+
+var SiteController = require('./controllers/SiteController')
 var app = express()
+
+app.get('/', SiteController.Index)
+module.exports = app
+app.get('/about', SiteController.About)
+app.get('/contact', SiteController.Contact)
 
 
 
 module.exports = app
-
-
-
-
-
-
-
-
-
-
